d82523fbb7ca06a7e1482720f0238adb5d0b198f
PilarSalaberry <pilarsalaberry@gmail.com>
2021-02-11 19:57:05 -0300

    Agrego funcionalidades y otros

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+let carrito = [];
+
+if (localStorage.getItem('carrito') != null) {
+  carrito = JSON.parse(localStorage.getItem('carrito'));
+  document.getElementById('cantidad').innerHTML = carrito.length;
+}
+
+actualizarCarrito();
+
 class Producto {
   constructor(
     idProducto,
@@ -16,7 +25,12 @@ class Producto {
   }
 }
 
-let listaProductos = [];
+class Carrito {
+  constructor(cantidadProductos, precioTotal) {
+    this.cantidad = cantidadProductos;
+    this.precio = precioTotal;
+  }
+}
 
 let producto1 = new Producto(1, 'Maceta 1', 500, 4, 'Maceta de Piedras');
 let producto2 = new Producto(2, 'Maceta 2', 600, 3, 'Maceta de Piedras');
@@ -24,13 +38,28 @@ let producto3 = new Producto(3, 'Maceta 3', 550, 5, 'Maceta de Cemento');
 let producto4 = new Producto(4, 'Maceta 4', 350, 2, 'Maceta de Cemento');
 let producto5 = new Producto(5, 'Maceta 5', 300, 1, 'Maceta de Cemento');
 
-listaProductos.push(producto1);
-listaProductos.push(producto2);
-listaProductos.push(producto3);
-listaProductos.push(producto4);
-listaProductos.push(producto5);
+let listaProductos = [producto1, producto2, producto3, producto4, producto5];
 
 let prod = ``;
+for (let i = 0; i < listaProductos.length; i++) {
+  prod += `
+  <div class="producto">
+  <img src="${
+    `img/Producto` + listaProductos[i].id + `.jpeg`
+  }" class="imagen" />
+  <h3>${listaProductos[i].nombre}</h3>
+  <p>${`$` + listaProductos[i].precio}</p>
+  <button class="btn" onclick="consultaStock(${listaProductos[i].id})">
+    Consultar Stock
+  </button>
+  <button class="btn" onclick='agregarAlCarrito(${JSON.stringify(
+    listaProductos[i]
+  )})'>Agregar al carrito</button>
+</div>
+  `;
+}
+
+document.getElementById('productos').innerHTML = prod;
 
 function consultaStock(idProducto) {
   let cantidad = Number(prompt('Ingrese la cantidad deseada'));
@@ -45,19 +74,42 @@ function consultaStock(idProducto) {
   }
 }
 
-for (let i = 0; i < listaProductos.length; i++) {
-  prod += `
-  <div class="producto">
-  <img src="${
-    `img/Producto` + listaProductos[i].id + `.jpeg`
-  }" class="imagen" />
-  <h3>${listaProductos[i].nombre}</h3>
-  <p>${`$` + listaProductos[i].precio}</p>
-  <button class="btn" onclick="consultaStock(${listaProductos[i].id})">
-    Consultar Stock
-  </button>
-</div>
-  `;
+function actualizarCarrito() {
+  let prodCarrito = ``;
+  for (let i = 0; i < carrito.length; i++) {
+    prodCarrito += `
+    <div class="cardCarrito">
+      <div class="agregados">
+        <h3>${carrito[i].nombre}</h3>
+        <p>${`$` + carrito[i].precio}</p>
+      </div>
+      <div>
+        <button class="x-carrito"><i class="fas fa-times"></i></button>
+      </div>
+    </div>
+    `;
+  }
+  document.getElementById('productosCarrito').innerHTML = prodCarrito;
 }
 
-document.getElementById('productos').innerHTML = prod;
+function agregarAlCarrito(nombreProducto) {
+  carrito.push(nombreProducto);
+  localStorage.setItem('carrito', JSON.stringify(carrito));
+  //sumar precio total
+  let suma = 0;
+  for (let i = 0; i < carrito.length; i++) {
+    suma += carrito[i].precio;
+  }
+  document.getElementById('precioTotal').innerHTML = 'Total: $' + suma;
+  document.getElementById('cantidad').innerHTML = carrito.length;
+  actualizarCarrito();
+}
+
+function vaciarCarrito() {
+  let nuevoCarrito = [];
+  localStorage.setItem('carrito', JSON.stringify(nuevoCarrito));
+  carrito = nuevoCarrito;
+  document.getElementById('cantidad').innerHTML = 0;
+  document.getElementById('precioTotal').innerHTML = 'Total:';
+  actualizarCarrito();
+}
